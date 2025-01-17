891dc739f6450c1360debc5b12ebe74b0f784e8b
Tyler <123605334+t3b0b@users.noreply.github.com>
2025-01-17 01:44:04 +0100

    Update pmg.py

--- blueprints/pmg.py
+++ blueprints/pmg.py
@@ -1,4 +1,4 @@
-qqqqqqqqqqqq from random import choice, random
+from random import choice, random
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify, flash, session
 from models import (User, db, Streak, Goals, Friendship, Notes, SharedItem, ActivityTracking, Notification,
                     Activity, Score, Dagar, ToDoList, TopFive, SubTask)
